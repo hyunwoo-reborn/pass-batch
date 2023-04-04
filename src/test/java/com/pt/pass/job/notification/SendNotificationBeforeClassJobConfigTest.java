e54e9d6996e90c70899d883e3a761ed98d827d14
hyuwnoo.park.reborn <hyunnwoo.park.reborn@gmail.com>
2023-04-05 01:18:11 +0900

    예정된 수업 전 알람(멀티 쓰레드)

---
+++ src/test/java/com/pt/pass/job/notification/SendNotificationBeforeClassJobConfigTest.java
@@ -0,0 +1,101 @@
+package com.pt.pass.job.notification;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.pt.pass.adapter.message.KakaoTalkMessageAdapter;
+import com.pt.pass.config.KakaoTalkMessageConfig;
+import com.pt.pass.config.TestBatchConfig;
+import com.pt.pass.repository.booking.BookingEntity;
+import com.pt.pass.repository.booking.BookingRepository;
+import com.pt.pass.repository.booking.BookingStatus;
+import com.pt.pass.repository.pass.PassEntity;
+import com.pt.pass.repository.pass.PassRepository;
+import com.pt.pass.repository.pass.PassStatus;
+import com.pt.pass.repository.user.UserEntity;
+import com.pt.pass.repository.user.UserRepository;
+import com.pt.pass.repository.user.UserStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@SpringBatchTest
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {
+        SendNotificationBeforeClassJobConfig.class,
+        TestBatchConfig.class,
+        SendNotificationItemWriter.class,
+        KakaoTalkMessageConfig.class,
+        KakaoTalkMessageAdapter.class
+})
+public class SendNotificationBeforeClassJobConfigTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private PassRepository passRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    public void test_addNotificationStep() throws Exception {
+        // given
+        addBookingEntity();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("addNotificationStep");
+
+        // then
+        assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+
+    }
+
+    private void addBookingEntity() {
+        final LocalDateTime now = LocalDateTime.now();
+        final String userId = "A100" + RandomStringUtils.randomNumeric(4);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(userId);
+        userEntity.setUserName("김영희");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setPhone("01033334444");
+        userEntity.setMeta(Map.of("uuid", "abcd1234"));
+        userRepository.save(userEntity);
+
+        PassEntity passEntity = new PassEntity();
+        passEntity.setPackageSeq(1);
+        passEntity.setUserId(userId);
+        passEntity.setStatus(PassStatus.PROGRESSED);
+        passEntity.setRemainingCount(10);
+        passEntity.setStartedAt(now.minusDays(60));
+        passEntity.setEndedAt(now.minusDays(1));
+        passRepository.save(passEntity);
+
+        BookingEntity bookingEntity = new BookingEntity();
+        bookingEntity.setPassSeq(passEntity.getPassSeq());
+        bookingEntity.setUserId(userId);
+        bookingEntity.setStatus(BookingStatus.READY);
+        bookingEntity.setStartedAt(now.plusMinutes(10));
+        bookingEntity.setEndedAt(bookingEntity.getStartedAt().plusMinutes(50));
+        bookingRepository.save(bookingEntity);
+
+    }
+
+}
